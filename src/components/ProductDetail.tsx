f82617a1cfa227c19eeb3b47e475aafcf7b1764d
nevraka <nevrakaya@gmail.com>
2021-07-28 19:56:30 +0200

    name change

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import styles from './Product.module.css';
+import styles from './ProductDetail.module.css';
+import Link from 'next/link';
 
 interface ProductProps {
   imageUrl: string;
   description: Function;
   title: string;
   price: number;
+  productId: string;
 }
 
-const Product = ({ imageUrl, description, title, price }: ProductProps) => {
+const Product = ({
+  productId,
+  imageUrl,
+  description,
+  title,
+  price,
+}: ProductProps) => {
   return (
     <div className={styles.container}>
       <div className={styles.main}>
         {imageUrl && <img src={imageUrl} className={styles.image} />}
         <div className={styles.compact}>
-          <h2 className={styles.heading}>{title}</h2>
+          <Link href={`/Product/${productId}`}>
+            <h2 className={styles.heading}>{title}</h2>
+          </Link>
           <span>{` € ${price}.00 Eur`}</span>
         </div>
       </div>
