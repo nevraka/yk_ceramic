1ea1de058b1501e688fe277c375fce663b8979fb
nevraka <nevrakaya@gmail.com>
2021-08-08 17:59:26 +0200

    added scroll event header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import styles from './Header.module.css';
-import profilePic from '../../public/profilePic.png';
+import logo from '../../public/logo.png';
 import Link from 'next/link';
 import withClient from '../utils/withClient';
 import { ContentfulClientApi } from 'contentful';
@@ -10,10 +10,12 @@ import LinkButton from './LinkButton';
 
 interface HeaderProps {
   client: ContentfulClientApi;
+  floating?: boolean;
 }
 
-const Header = ({ client }: HeaderProps) => {
+const Header = ({ client, floating = false }: HeaderProps) => {
   const [categories, setCategories] = useState<any[]>([]);
+  const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
     const getCategories = async () => {
@@ -23,49 +25,67 @@ const Header = ({ client }: HeaderProps) => {
     getCategories();
   }, []);
 
+  useEffect(() => {
+    const onScroll: EventListener = (e: Event) => {
+      const top = (e.target as Document).documentElement.scrollTop;
+      setIsScrolled(top !== 0);
+    };
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
+  const classes = [styles.header];
+  if (isScrolled) {
+    classes.push(styles.scrolled);
+  }
+
   return (
-    <div className={styles.header}>
-      <Link href="/">
-        <a>
-          <Image
-            width={100}
-            height={100}
-            className={styles.logo}
-            src={profilePic}
-            alt="Picture of the brand"
-          />
-        </a>
-      </Link>
-      <div className={styles.categories}>
-        {categories.map((category) => (
+    <>
+      {!floating && <div className={styles.placeholder} />}
+      <div className={classes.join(' ')}>
+        <Link href="/">
+          <a>
+            <Image
+              width={isScrolled ? 50 : 100}
+              height={isScrolled ? 50 : 100}
+              className={styles.logo}
+              src={logo}
+              alt="Picture of the brand"
+            />
+          </a>
+        </Link>
+        <div className={styles.categories}>
+          {categories.map((category) => (
+            <LinkButton
+              key={category.sys.id}
+              className={styles.category}
+              href={`/Category/${category.sys.id}`}
+            >
+              {category.fields.title}
+            </LinkButton>
+          ))}
+        </div>
+        <div className={styles.right}>
+          <LinkButton href={'/GiftCard'}>Gift Card</LinkButton>
           <LinkButton
-            key={category.sys.id}
-            className={styles.category}
-            href={`/Category/${category.sys.id}`}
+            className={styles.icon}
+            href="https://www.instagram.com/yakaart_ceramic/?hl=en"
           >
-            {category.fields.title}
+            <GrInstagram />
           </LinkButton>
-        ))}
+          <LinkButton
+            className={styles.icon}
+            href="https://www.shopier.com/ShowProductNew/storefront.php?shop=yakaart&sid=NlN4b0xtZlBhclJWWHR4ZTBfLTFfIF8g"
+          >
+            <GrCart />
+          </LinkButton>
+          <LinkButton className={styles.category} href="/AboutMe">
+            About me
+          </LinkButton>
+        </div>
       </div>
-      <div className={styles.right}>
-        <LinkButton href={'/GiftCard'}>Gift Card</LinkButton>
-        <LinkButton
-          className={styles.icon}
-          href="https://www.instagram.com/yakaart_ceramic/?hl=en"
-        >
-          <GrInstagram />
-        </LinkButton>
-        <LinkButton
-          className={styles.icon}
-          href="https://www.shopier.com/ShowProductNew/storefront.php?shop=yakaart&sid=NlN4b0xtZlBhclJWWHR4ZTBfLTFfIF8g"
-        >
-          <GrCart />
-        </LinkButton>
-        <LinkButton className={styles.category} href="/AboutMe">
-          About me
-        </LinkButton>
-      </div>
-    </div>
+    </>
   );
 };
 
