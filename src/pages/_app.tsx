c47ce52ffba7ea71f6f9189b7d46aeaf761a15cd
nevraka <nevrakaya@gmail.com>
2021-08-17 10:49:51 +0200

    nprogress and style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,30 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import type { AppProps } from 'next/app';
 import Footer from '../components/Footer';
+import Router from 'next/router';
+import Head from 'next/head';
+import NProgress from 'nprogress';
+NProgress.configure({ showSpinner: false });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  Router.events.on('routeChangeStart', (url) => {
+    NProgress.start();
+  });
+
+  Router.events.on('routeChangeComplete', (url) => {
+    NProgress.done();
+  });
   return (
     <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
