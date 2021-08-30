82ba8812f6c46b1e646cec34de31f90b9d0df2a7
nevraka <nevrakaya@gmail.com>
2021-08-30 17:54:40 +0200

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      inital: ['initial'],
+    },
+    textColor: {
+      '404color': '#106f86',
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  },
+};
