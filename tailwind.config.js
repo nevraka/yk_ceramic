95bd93516b0ba21e6d9b24f731fda1a13aa3e710
nevraka <nevrakaya@gmail.com>
2021-08-31 17:45:48 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,30 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      inital: ['initial'],
+    extend: {
+      fontFamily: {
+        inital: ['initial'],
+      },
+      textColor: {
+        '404color': '#106f86',
+        footertext: '#4f4f4f',
+        compactcolor: '#444',
+      },
+      backgroundColor: {
+        footer: '#fafafa',
+      },
+      borderColor: {
+        ftborder: '#e5e5e5',
+      },
+      boxShadow: {
+        productdetail:
+          '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);',
+      },
     },
-    textColor: {
-      '404color': '#106f86',
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
   },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
