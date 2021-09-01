6952e325f8dc3d2a28b3eada5b5cffc130885ba0
nevraka <nevrakaya@gmail.com>
2021-09-01 20:56:41 +0200

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
@@ -10,9 +12,11 @@ module.exports = {
         '404color': '#106f86',
         footertext: '#4f4f4f',
         compactcolor: '#444',
+        linkButton: '#106f86',
       },
       backgroundColor: {
         footer: '#fafafa',
+        scrolled: 'rgba(255, 255, 255, 0.75)',
       },
       borderColor: {
         ftborder: '#e5e5e5',
@@ -21,6 +25,29 @@ module.exports = {
         productdetail:
           '0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);',
       },
+      zIndex: {
+        1: 1,
+      },
+      transitionProperty: {
+        backgroundColor: 'backgroundColor',
+        padding: 'padding',
+      },
+    },
+    screens: {
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
     },
   },
 
